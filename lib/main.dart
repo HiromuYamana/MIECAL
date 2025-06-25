3658a2c6521c2c7ee6022b3d0fb8c7233007e2e6
HiromuYamana <e19233094@oit.ac.jp>
2025-06-25 19:24:12 +0900

    空のページと画面遷移を作りました。

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MIECAL',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(title: 'MIECAL'),
+        '/LoginPage': (context) => const LoginPage(),
+        '/SymptomPage': (context) => const SymptomPage(),
+        '/AffectedAreaPage': (context) => const AffectedAreaPage(),
+        '/DatePage': (context) => const DatePage(),
+        '/SufferLevelPage': (context) => const SufferLevelPage(),
+        '/CousePage': (context) => const CousePage(),
+        '/OtherInfomationPage': (context) => const OtherInfomationPage(),
+        '/QuestionnairePage': (context) => const QuestionnairePage(),
+        
+      },
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
       ),
-      home: const MyHomePage(title: 'MIECAL'),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -30,23 +41,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _showQrCodePage() {
-    final currentUrl = html.window.location.href;
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => QrPage(data: currentUrl),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,31 +53,169 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
             const SizedBox(height: 30),
             ElevatedButton(
-              onPressed: _showQrCodePage,
-              child: const Text('QRコード作成 Making QR'),
+              onPressed: () => Navigator.pushNamed(context, '/LoginPage'),
+              child: const Text('Next'),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
 }
 
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ログイン・新規登録')),
+      body: Center(
+        child:ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/SymptomPage'),
+          child: const Text('Next'),
+        ),
+      ),
+    );
+  }
+}
+
+class SymptomPage extends StatelessWidget {
+  const SymptomPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('症状入力')),
+        body: Center(
+        child:ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/AffectedAreaPage'),
+          child: const Text('Next'),
+        ),
+      ),
+    );
+  }
+}
+
+class AffectedAreaPage extends StatelessWidget {
+  const AffectedAreaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('患部')),
+      body: Center(
+        child:ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/DatePage'),
+          child: const Text('Next'),
+        ),
+      ),
+    );
+  }
+}
+
+class DatePage extends StatelessWidget {
+  const DatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('日付')),
+      body: Center(
+        child:ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/SufferLevelPage'),
+          child: const Text('Next'),
+        ),
+      ),
+    );
+  }
+}
+
+class SufferLevelPage extends StatelessWidget {
+  const SufferLevelPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('程度選択')),
+      body: Center(
+        child:ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/CousePage'),
+          child: const Text('Next'),
+        ),
+      ),
+    );
+  }
+}
+
+class CousePage extends StatelessWidget {
+  const CousePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('原因')),
+      body: Center(
+        child:ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/OtherInfomationPage'),
+          child: const Text('Next'),
+        ),
+      ),
+    );
+  }
+}
+
+class OtherInfomationPage extends StatelessWidget {
+  const OtherInfomationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('その他情報入力')),
+      body: Center(
+        child:ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/QuestionnairePage'),
+          child: const Text('Next'),
+        ),
+      ),
+    );
+  }
+}
+
+class QuestionnairePage extends StatelessWidget {
+  const QuestionnairePage({super.key});
+
+  void _showQrCodePage(BuildContext context) {
+    final currentUrl = html.window.location.href;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => QrPage(data: currentUrl),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('問診表')),
+      body: Center(
+        child:ElevatedButton(
+          onPressed: () => _showQrCodePage(context),
+          child: const Text('QRコード作成 Create QRcode'),
+        ),
+      ),
+    );
+  }
+}
+
+
+
 class QrPage extends StatelessWidget {
   final String data;
-
   const QrPage({super.key, required this.data});
 
   @override
@@ -100,8 +232,8 @@ class QrPage extends StatelessWidget {
                 data: data,
                 version: QrVersions.auto,
                 size: 200.0,
-                ),   
-              ),
+                ),
+              )
             ),
             Expanded(
                 child: Text('ご記入ありがとうございました。',style: TextStyle(fontSize: 30))
